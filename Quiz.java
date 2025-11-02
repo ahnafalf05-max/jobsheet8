40bbf344bc18aba89234499e3951886da13ca241
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-02 18:55:54 +0700

    first

--- Quiz.java
+++ Quiz.java
@@ -6,6 +6,29 @@ public class Quiz {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
+        char menu = 'y';
+
+        do {
+            int number = rand.nextInt(10) + 1; 
+            boolean success = false;
+
+            do {
+                System.out.print("Guess the number (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine(); // 
+                success = (answer == number);
+            } while (!success);
+
+            System.out.println("Do you want to repeat the game (Y/N) :");
+            menu = input.next().charAt(0);
+            input.nextLine(); // 
+        } while (menu == 'Y' || menu == 'y');
+
         
+        input.close();
     }
 }
+
+
+    
+
