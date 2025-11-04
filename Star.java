3fa0ad9945d3272dc4ce079b40708a25eefd93be
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 16:41:40 +0700

    questions 1

--- Star.java
+++ Star.java
@@ -5,7 +5,7 @@ public class Star {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of N: ");
         int N = sc.nextInt();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             System.out.print("*");
         }
         System.out.println();
--- Triangle.java
+++ Triangle.java
@@ -6,13 +6,13 @@ public class Triangle {
         System.out.print("Enter the value of N: ");
         int N = sc.nextInt();
 
-        // Loop baris
+        
         for (int i = 1; i <= N; i++) {
-            // Loop kolom: cetak sebanyak nilai baris
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            // Pindah ke baris baru
+            
             System.out.println();
         }
     }
