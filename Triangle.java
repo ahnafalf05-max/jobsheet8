16074596dbeb93fff34bbed00cb9dadfb7ae5872
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 18:39:14 +0700

    commit

--- Triangle.java
+++ Triangle.java
@@ -17,3 +17,12 @@ public class Triangle {
         }
     }
 }
+
+/*
+1.Look at the results, does the output produced with a value of N = 5 match the following 
+display?  
+output : Each row  contains exactly  asterisks, from 1 to 5
+2.If not, which parts should be improved or added? Describe any parts that need to be 
+improved or added!
+Since the output does match the expected left aligned triangle when n = 5, the program is functionally correct
+*/
