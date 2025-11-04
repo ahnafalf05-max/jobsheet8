6fa627c6ee63902c6097a0d64b173c40fedd5ed2
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 18:48:43 +0700

    commit

--- NestedLoop1.java
+++ NestedLoop1.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class NestedLoop1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double[][] temps = new double[5][7]; 
+        double[][] temps = new double[5][7];
 
-        
+        // Input temperatures
         for (int i = 0; i < temps.length; i++) {
             System.out.println("City " + i + ":");
             for (int j = 0; j < temps[0].length; j++) {
@@ -16,15 +16,30 @@ public class NestedLoop1 {
         }
 
         
-        
-        for (int i = 0; i < temps.length; i++) {
-            System.out.print("City " + i + ": ");
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+        int cityIndex = 0;
+        for (double[] cityTemps : temps) {
+            System.out.print("City " + cityIndex + ": ");
+            for (double temp : cityTemps) {
+                System.out.print(temp + " ");
             }
             System.out.println();
+            cityIndex++;
         }
 
         scanner.close();
     }
 }
+
+/* 
+1.Explain the program flow in Experiment 5! 
+To collect and display temperature data for 5 cities over 7 days using a 2D array and nested loops
+2.Modify the program to display an array using foreach! 
+Done
+3. Modify the program so that it can display the average value for each city!
+
+
+
+
+
+
+*/
